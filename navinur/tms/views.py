ee03e1ec9526f652303ced7f938087758be59a3a
LiliyaStefanova <liliya.stefanova@gmail.com>
2015-07-09 17:13:36 +0100

    dummy implementation of view methods for tms app

--- navinur/tms/views.py
+++ navinur/tms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Views for the tile map server
+
+from django.http import HttpResponse
+
+def root(request):
+    return HttpResponse("Dummy tile map server implementation for testing")
+
+def service(request, version):
+    return HttpResponse("Dummy tile map service implementation for testing ")
+
+def tileMap(request, version, area):
+    return HttpResponse("Dummy service to display the whole tile map ")
+
+def tile(request, version, area, zoom, x, y):
+    return HttpResponse("Dummy impelementation of a tile displayed based on coords")
+
+
+
