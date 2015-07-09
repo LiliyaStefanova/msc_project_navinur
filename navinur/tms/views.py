965121a53eb8230989c75f1e136e65fa4d3a8c87
LiliyaStefanova <liliya.stefanova@gmail.com>
2015-07-09 17:23:26 +0100

    implement xml for map server to be accessed by Openlayers

--- navinur/tms/views.py
+++ navinur/tms/views.py
@@ -3,12 +3,29 @@ from django.shortcuts import render
 # Views for the tile map server
 
 from django.http import HttpResponse
+from django.http import Http404
+import traceback
 
 def root(request):
-    return HttpResponse("Dummy tile map server implementation for testing")
+    try:
+        baseURL = request.build_absolute_uri()
+        xml = []
+        xml.append('<?xml version = "1.0" encoding="utf-8" ?>')
+        xml.append('<Services>')
+        xml.append('    <TileMapService ' +
+                   'title = "Navinur tile map service" ' +
+                   'version = "1.0" href=" ' + baseURL + '1.0" />')
+        xml.append('</Services>')
+        return HttpResponse("\n".join(xml), content_type="text/xml")
+    except:
+        traceback.print_exc()
+        return HttpResponse("Error")
+
 
 def service(request, version):
-    return HttpResponse("Dummy tile map service implementation for testing ")
+        #creation of a service xml file which will be accessed by Openlayers
+        return HttpResponse("Dummy map service")
+
 
 def tileMap(request, version, area):
     return HttpResponse("Dummy service to display the whole tile map ")
