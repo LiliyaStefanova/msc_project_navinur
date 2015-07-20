f60a65967facc9eac0c4f284026756f5ed6ecf8d
LiliyaStefanova <liliya.stefanova@gmail.com>
2015-07-20 23:11:00 +0100

    updated models to remove approach scale; updated fixture to add ocean base layer and updated map file with additional styles

--- navinur/tms/models.py
+++ navinur/tms/models.py
@@ -14,3 +14,15 @@ class BaseMap(models.Model):
     class Meta:
         managed = True
         db_table = 'tms_basemap'
+
+
+class Ne50MOcean(models.Model):
+    gid = models.AutoField(primary_key=True)
+    scalerank = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
+    featurecla = models.CharField(max_length=32, blank=True, null=True)
+    geom = models.MultiPolygonField(blank=True, null=True)
+    objects = models.GeoManager()
+
+    class Meta:
+        managed = False
+        db_table = 'ne_50m_ocean'
